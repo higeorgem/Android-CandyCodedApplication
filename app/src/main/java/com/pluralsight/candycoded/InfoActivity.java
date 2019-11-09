dfe17957cd2b2a1cff251e4c81ed82a5e26139ac
higeorgem <higeorgem@gmail.com>
2019-11-09 23:27:46 +0300

    start activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent actionDial=new Intent(Intent.ACTION_DIAL);
+        Uri tel=Uri.parse("tel:0123456789");
+        actionDial.setData(tel);
+        startActivity(actionDial);
+    }
 }
