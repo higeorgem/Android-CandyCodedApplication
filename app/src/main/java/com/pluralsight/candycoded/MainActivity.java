f7f0ac208a18ba9fd5d834a20d35d9d87109a8f6
higeorgem <higeorgem@gmail.com>
2019-11-09 22:56:54 +0300

    start activity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent infoIntent =  new Intent(this,InfoActivity.class);
+        startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
     }
     // ***
